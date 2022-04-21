578af235c1a6048dfb257cb5dc1276a134d6969c
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-21 17:52:39 +0200

    day14/task1 "numbers.txt" add and fill with ten numbers

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,7 @@
 package day14;
 /*
 Create a ".txt" file in the project folder.
-Fill it in manually with ten arbitrary numbers.txt. Implement a static method printSumDigits(File file) that calculates the
+Fill it in manually with ten arbitrary numbers. Implement a static method printSumDigits(File file) that calculates the
 sum of all numbers.txt in this file and prints it to the screen. Call this method in the main() method.
 If the file does not exist in the project folder, the program must throw an exception and output the message “File not
 found” to the console. In addition, if the numbers.txt in the file are less than or greater than 10, it is necessary to
