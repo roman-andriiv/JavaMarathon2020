4b8e8f4fea6bae13ee13c6e2c0ef53b00c9df39b
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-21 17:49:05 +0200

    day14/task1 Task`s description add

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,5 +1,12 @@
 package day14;
-
+/*
+Create a ".txt" file in the project folder.
+Fill it in manually with ten arbitrary numbers.txt. Implement a static method printSumDigits(File file) that calculates the
+sum of all numbers.txt in this file and prints it to the screen. Call this method in the main() method.
+If the file does not exist in the project folder, the program must throw an exception and output the message “File not
+found” to the console. In addition, if the numbers.txt in the file are less than or greater than 10, it is necessary to
+throw an exception and display the message “Incorrect input file” in the console.
+*/
 public class Task1 {
     public static void main(String[] args) {
 
