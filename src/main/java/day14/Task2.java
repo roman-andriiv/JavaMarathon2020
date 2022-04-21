0f49955effa16a2da60a6f46b5ca9e64be74e038
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-21 22:39:23 +0200

    day14/task2 task's description add

--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,5 +1,10 @@
 package day14;
-
+/*Create a file "people.txt" in the project folder, which stores the names and ages of people.
+* Implement a List<String> static parseFileToStringList() method that reads the contents of this file and returns a
+* list of each person's name and age values. Get this list in the main() method and print it to the console.
+* If the file does not exist in the project folder, the program must throw an exception and output the message
+* “File not found” to the console. In addition, if the age value is negative, it is necessary to throw an exception
+* and print the message “Invalid input file” to the console.*/
 public class Task2 {
     public static void main(String[] args) {
 
