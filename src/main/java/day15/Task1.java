bd86069f5ffa9d07235ba16e85ae34bd04106bb5
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-25 16:09:48 +0200

    day15/task1 description add

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,5 +1,14 @@
 package day15;
-
+/*
+    Given a file with information about the remaining shoes in the warehouse (src/main/resources/shoes.csv) . This is
+an example of a real unloading of residues from 1C to a ".csv" file (file format with separators, the symbol “;” is
+used as a separator). This file contains information about the name of the shoe model, its size and the quantity
+remaining in stock.
+    Before converting to csv it was a table with three columns.
+It is necessary to generate a new file (src/main/resources/missing_shoes.txt) with information about models and sizes
+of shoes that are not in stock (quantity = 0) . To do this, implement a program that takes a ".csv" file as input and
+creates a new ".txt" file.
+*/
 public class Task1 {
     public static void main(String[] args) {
 
