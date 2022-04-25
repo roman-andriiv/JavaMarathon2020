1ccd4c8c93daffc69743197791730e2ba86187b7
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-25 11:25:28 +0200

    day14/task3 description add

--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,5 +1,12 @@
 package day14;
+/*
+Create a class “Person” with fields “name” and “age”. Implement a static List<Person> parseFileToObjList() method that
+reads the contents of the same "people.txt" file, creates instances of the Person class, and returns a list of objects.
+Get this list in the main() method and print it to the console.
 
+If the file does not exist in the project folder, the program must throw an exception and output the message
+“File not found” to the console. In addition, if the age value is negative, it is necessary to throw an exception and
+print the message “Invalid input file” to the console.*/
 public class Task3 {
     public static void main(String[] args) {
 
