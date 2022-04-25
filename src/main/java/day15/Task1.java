2dd38bc19cb701ceb65aabcbc5715aedd9a86d96
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-25 17:15:25 +0200

    day15/task1 solution add

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,4 +1,10 @@
 package day15;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /*
     Given a file with information about the remaining shoes in the warehouse (src/main/resources/shoes.csv) . This is
 an example of a real unloading of residues from 1C to a ".csv" file (file format with separators, the symbol “;” is
@@ -10,7 +16,23 @@ of shoes that are not in stock (quantity = 0) . To do this, implement a program 
 creates a new ".txt" file.
 */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/shoes.csv");
+        Scanner scanner = new Scanner(file);
 
+        File txtFile = new File("src/main/resources/missing_shoes.txt");
+        PrintWriter writer = new PrintWriter(txtFile);
+
+        int counter = 1;
+        while (scanner.hasNextLine()) {
+            String[] wordsArr = scanner.nextLine().split(";");
+            if (Integer.parseInt(wordsArr[2]) == 0) {
+                writer.println(counter+". "+ wordsArr[0] + "; " + wordsArr[1] + "; " + wordsArr[2]);
+                counter++;
+            }
+        }
+        writer.close();
+        scanner.close();
+        System.out.println("file is created");
     }
 }
