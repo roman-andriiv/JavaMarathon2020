b6e39d70e35bff28a9862201377805b1d8ec96b1
Roman Andriiv <roman.andriyiv@gmail.com>
2022-04-26 12:26:48 +0200

    day16/task2 description add

--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,5 +1,14 @@
 package day16;
+/*Implement a program that writes numbers of different types to 2 files. File 1 should be called “file1.txt” and File 2
+should be called “file2.txt”. Both files must be in the root of the project. Files should not be created manually, but
+when the program is started. File 1 must contain 1000 random numbers from 0 to 100. File 2 is based on File 1 but
+contains numbers of the real data type.
 
+The method for filling File 2 is as follows: for each group of 20 integers from File 1, their arithmetic mean is
+calculated. Then, the resulting value is written to File 2. Thus, File 2 will contain 50 real numbers (1000/20 = 50).
+
+After File 2 has been generated, the static method printResult(File file) must be implemented. This method should
+calculate the sum of all real numbers from File 2 and print it to the console, discarding the real part.*/
 public class Task2 {
     public static void main(String[] args) {
 
